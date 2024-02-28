2b27a3edfa6ee06808766e74e816e3d25d65ccf7
kenntnisse <72457920+kenntnisse@users.noreply.github.com>
2024-02-28 23:11:42 +0000

    added room

---
+++ Cave Test/Room.java
@@ -0,0 +1,26 @@
+public class Room {
+  Room[] adj;
+  int id;
+  public Room(){
+    
+  }
+  public Room(int i, Room[] a){
+    this.adj = a;
+    this.id = i;
+  }
+  public Room(int i, Room a, Room b, Room c){
+    this.adj[0] = a;
+    this.adj[1] = b;
+    this.adj[2] = c;
+    this.id = i;
+  }
+
+  @Override
+  public String toString(){
+    return ""+id;
+  }
+
+  public String paths(){
+    return this.adj[0]+", "+this.adj[1]+", "+this.adj[2];
+  }
+}
