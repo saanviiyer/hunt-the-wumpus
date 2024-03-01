c4ef71167249e3a99dc0878d467bd30c55b24dd6
kenntnisse <72457920+kenntnisse@users.noreply.github.com>
2024-03-01 12:49:25 -0800

    today

--- Cave Test/Room.java
+++ Cave Test/Room.java
@@ -2,7 +2,9 @@ public class Room {
   Room[] adj;
   int id;
   public Room(){
-    
+  }
+  public Room(int i){
+    this.id = i;
   }
   public Room(int i, Room[] a){
     this.adj = a;
@@ -13,6 +15,13 @@ public class Room {
     this.adj[1] = b;
     this.adj[2] = c;
     this.id = i;
+  }
+
+  public void setAdj(Room[] a){
+    this.adj = a;
+  }
+  public void setAdj(Room a, Room b, Room c){
+
   }
 
   @Override
