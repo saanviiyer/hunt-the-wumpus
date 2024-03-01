0ca1812d11b211fabcc9ba509cc9f126d4b71753
saanviiyer <saanvi.iyer@icloud.com>
2024-03-01 13:03:57 -0800

    finished game control empty

--- GameControl/GameControl.java
+++ GameControl/GameControl.java
@@ -2,7 +2,15 @@
 // Game Control
 
 // new change
+// Handles user input (except for High Score and Trivia), coordinates all the other parts of the game.
 
 public class GameControl {
+
+// work with UI object to start the game and display the current room.
+
+    public GameControl() {
+        
+    }
+
 
 }
