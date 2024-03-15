aecd37f50738c00fbd1d78e49e2a6c28735a4abf
Yana Ivanov <2012769@apps.nsd.org>
2024-03-14 19:05:26 -0700

    Added HighScore class implementation and the test version

---
+++ HighScore/HighScoreTest.java
@@ -0,0 +1,60 @@
+public class HighScoreTest {
+
+    public static void testResetScores() {
+        HighScore highScore = new HighScore();
+        highScore.resetScores();
+        if (highScore.getScore() == 0) {
+            System.out.println("testResetScores: Success");
+        } else {
+            System.out.println("testResetScores: Failure");
+        }
+    }
+
+    public static void testIncrementScore() {
+        HighScore highScore = new HighScore();
+        highScore.incrementScore(10);
+        if (highScore.getScore() == 10) {
+            System.out.println("testIncrementScore: Success");
+        } else {
+            System.out.println("testIncrementScore: Failure");
+        }
+    }
+
+    public static void testDecrementScore() {
+        HighScore highScore = new HighScore();
+        highScore.incrementScore(10); // Increment first to avoid direct reset
+        highScore.decrementScore(5);
+        if (highScore.getScore() == 5) {
+            System.out.println("testDecrementScore: Success");
+        } else {
+            System.out.println("testDecrementScore: Failure");
+        }
+    }
+
+    public static void testDecrementScoreBelowZero() {
+        HighScore highScore = new HighScore();
+        highScore.decrementScore(1); // This should reset the score to 0 due to negative value
+        if (highScore.getScore() == 0) {
+            System.out.println("testDecrementScoreBelowZero: Success");
+        } else {
+            System.out.println("testDecrementScoreBelowZero: Failure");
+        }
+    }
+
+    public static void testGetScore() {
+        HighScore highScore = new HighScore();
+        if (highScore.getScore() == 0) {
+            System.out.println("testGetScore: Success");
+        } else {
+            System.out.println("testGetScore: Failure");
+        }
+    }
+
+    public static void main(String[] args) {
+        testResetScores();
+        testIncrementScore();
+        testDecrementScore();
+        testDecrementScoreBelowZero();
+        testGetScore();
+    }
+}
