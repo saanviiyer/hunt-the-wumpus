2d6b26c12cd5800d7558317b2f1571bf3cfab73f
kenntnisse <72457920+kenntnisse@users.noreply.github.com>
2024-03-19 12:55:36 -0700

    cleaned GameLocations

--- GameLocations/GameLocations.java
+++ GameLocations/GameLocations.java
@@ -4,6 +4,7 @@
 // saanvi doing pavan's work
 package GameLocations;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GameLocations {
 
@@ -16,51 +17,56 @@ public class GameLocations {
     int shopPos;
 
     // CONSTRUCTOR
-    public GameLocation() {
-        // assign each property to a random value
+    public GameLocations() {
+        this.wumpusPos = 0;
+        this.playerPos = 15;
+        this.pitPos = new int[0];
+        this.batPos = new int[0];
+        this.fallenArrows = new ArrayList<Integer>();
+        this.shopPos = 2;
     }
 
     // METHOD (add getters and setters)
 
     public int getWumpusPos() {
-        return wumpusPos;
+        return this.wumpusPos;
     }
 
-    public setWumpusPos(int pos) {
-        wumpusPos = pos;
+    public void setWumpusPos(int pos) {
+        this.wumpusPos = pos;
     }
 
     public int getPlayerPos() {
-        return playerPos;
+        return this.playerPos;
     }
 
-    public setPlayerPos(int pos) {
-        playerPos = pos;
+    public void setPlayerPos(int pos) {
+        this.playerPos = pos;
     }
 
     public int[] getPitPos() {
-        return pitPos;
+        return this.pitPos;
     }
 
-    public setPitPos(int[] pos) {
-        pitPos = pos;
+    public void setPitPos(int[] pos) {
+        this.pitPos = pos;
     }
 
     public int[] getBatPos() {
-        return batPos;
+        return this.batPos;
     }
 
-    public setBatPos(int[] pos) {
-        batPos = pos;
+    public void setBatPos(int[] pos) {
+        this.batPos = pos;
     }
 
 
 
     public int getShopPos() {
-        return shopPos;
+        return this.shopPos;
     }
 
     public ArrayList<Integer> getFallenArrows() {
-        return fallenArrows;
+        return this.fallenArrows;
     }
 }
