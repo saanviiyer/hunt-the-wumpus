c1e8620356ec1db5a7852e34df1eeeab7bde0830
kenntnisse <72457920+kenntnisse@users.noreply.github.com>
2024-03-19 13:11:29 -0700

    hexes know their adjacencies

--- Cave/Hex.java
+++ Cave/Hex.java
@@ -8,25 +8,27 @@ public class Hex extends JButton{
   static final int LENGTH = 50; // side length, in pixels
   // radius to an edge is sqrt3 * length/2
   // radius to a vertex is length
-  
-  static int nRows = 5;
-  static int nCols = 6;
+  static final Color WHITE = new Color(255,255,255);
+  static final Color BLACK = new Color(0,0,0);
+  static final Color RED = new Color(255,0,0);
+  static final Color GREEN = new Color(0,255,0);
+  static final Color BLUE = new Color(0,0,255);
+
+  static final int nRows = 5;
+  static final int nCols = 6;
   int row;
   int col;
   int id;
+  double x, y;
   Color color = new Color(255,255,255);
   Polygon hex;
+  int adj[] = new int[6];
+
   public Hex(int r, int c){
-    //super(""+r+",");
-    super(""+(r*6+c));
-    this.id = r*6+c;
+    super(""+(r*nCols+c));
+    this.id = r*nCols+c;
     this.row = r;
     this.col = c;
-    double x = this.col*1.5*LENGTH+LENGTH;
-    double y = this.row*LENGTH*1.732+LENGTH;
-    if (this.col%2 == 1) y += LENGTH*1.732/2;
-    setBounds((int)x, (int)y, (int)LENGTH*2, (int)(LENGTH*1.732+2));
-    //setSize((int)LENGTH*2, (int)(LENGTH*1.732+2));
     setContentAreaFilled(false);
     setFocusPainted(true);
     setBorderPainted(false);
@@ -35,32 +37,37 @@ public class Hex extends JButton{
     for (double[] i: this.getPoints()){
       this.hex.addPoint((int)(i[0]), (int)(i[1]));
     }
-    //setLocation((int)x, (int)y);
-    //setSize((int)LENGTH, (int)(LENGTH));
+    
+    this.x = this.col*1.5*LENGTH+LENGTH;
+    this.y = this.row*LENGTH*1.732+LENGTH;
+    if (this.col%2 == 1) this.y += LENGTH*1.732/2;
+    setBounds((int)this.x, (int)this.y, (int)LENGTH*2, (int)(LENGTH*1.732+2));
+
+    // columns are aligned by even/oddness
+    if (col%2 == 1){ // lower columns
+        this.adj[0] = ( ((row-1+5)%5)*6 +  col        )%30; // previous row, same column
+        this.adj[1] = (   row        *6 + (col+1)  %6 )%30; // same row, next column
+        this.adj[2] = ( ((row+1)  %5)*6 + (col+1)  %6 )%30; // next row, next column
+        this.adj[3] = ( ((row+1)  %5)*6 +  col        )%30; // next row, same column
+        this.adj[4] = ( ((row+1)  %5)*6 + (col-1+6)%6 )%30; // next row, previous column
+        this.adj[5] = (   row        *6 + (col-1+6)%6 )%30; // same row, previous column
+    } else { // upper columns
+        this.adj[0] = ( ((row-1+5)%5)*6 +  col        )%30; // previous row, same column
+        this.adj[1] = ( ((row-1+5)%5)*6 +  (col+1)%6  )%30; // previous row, next column
+        this.adj[2] = (   row        *6 + (col+1)  %6 )%30; // same row, next column
+        this.adj[3] = ( ((row+1)  %5)*6 +  col        )%30; // next row, same column
+        this.adj[4] = (   row        *6 + (col-1+6)%6 )%30; // same row, previous column
+        this.adj[5] = ( ((row-1+5)%5)*6 + (col-1+6)%6 )%30; // previous row, previous column
+    }
   }
+
   public Hex(int i){
-    this(i/6, i%6);
+    this(i/nCols, i%nCols);
   }
-  public static void setDim(int nr, int nc){
-    nRows = nr;
-    nCols = nc;
-  }
+
 
   public double[][] getPoints(){
-    /*
-    double[][] points = new double[4][2];
-    points[0][0] = 0;
-    points[0][1] = 0;
 
-    points[1][0] = LENGTH;
-    points[1][1] = 0;
-
-    points[2][0] = LENGTH;
-    points[2][1] = LENGTH;
-
-    points[3][0] = 0;
-    points[3][1] = LENGTH;
-    /*/
     double[][]points = new double[6][2];
     // x, y
     points[0][0] = LENGTH * 0.5;
@@ -85,13 +92,12 @@ public class Hex extends JButton{
   }
 
   public void setColor(Color c){this.color = c; this.repaint();}
-  public void reset(){this.setColor(new Color(255,255,255));}
+
+  public void reset(){this.setColor(WHITE);}
+
   @Override
   public void paintComponent(Graphics g){
     super.paintComponent(g);
-    double x = this.col*1.5*LENGTH+LENGTH;
-    double y = this.row*LENGTH*1.732+LENGTH;
-    if (this.col%2 == 1) y += LENGTH*1.732/2;
     setBounds((int)x, (int)y, (int)LENGTH*2, (int)(LENGTH*1.732+2));
     g.drawPolygon(this.hex);
     
@@ -101,6 +107,7 @@ public class Hex extends JButton{
     g.setColor(new Color(0,0,0));
     g.drawString(""+(this.row*6+this.col), (int) (LENGTH), (int)(LENGTH*1.732/2));
   }
+
   @Override
   public boolean contains(int x, int y){
     return this.hex.contains(x, y);
