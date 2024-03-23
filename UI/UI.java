28bd78d9e6e52b4e04c0c91ff3e06e64c151db12
nathanc32 <2030699@apps.nsd.org>
2024-03-23 15:49:22 -0700

    ui rework, change to gridbag, formatting, arrow label

--- UI/UI.java
+++ UI/UI.java
@@ -9,22 +9,29 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class UI extends JFrame implements ActionListener{
-    int var;
+    int var = 0;
     String permString = "Var equals: ";
     JMenuBar menuBar = new JMenuBar();
     JMenu menu = new JMenu("File");
     JButton button = new JButton("Increase var");
-    JLabel label = new JLabel(permString);
+    JLabel label = new JLabel(permString + var);
     JMenuItem exit = new JMenuItem("Exit");
 
     JTextField textField = new JTextField(10);
     JButton submitText = new JButton("Submit Text");
 
     JMenuItem startNewGame = new JMenuItem("New Game");
+
+    JTextField roomInput = new JTextField("enter room number");
     JButton move = new JButton("Move");
+
     JButton shoot = new JButton("shoot");
+
     JButton buyArrows = new JButton("Purchase Arrows");
     JButton buySecrets = new JButton("Purchase Secrets");
+
+    int arrows = 0;
+    JLabel arrowLabel = new JLabel("Arrows: " + arrows);
    
     ////////////////////////
     ////   CONSTRUCTOR  ////
@@ -37,11 +44,21 @@ public class UI extends JFrame implements ActionListener{
         setTitle("Hunt the Wumpus");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1024,576);
-        setLayout(new GridLayout(3,3));
+        setLayout(new GridBagLayout());
         
+        //change icon of frame
         ImageIcon icon = new ImageIcon("wumpus4.png");
         setIconImage(icon.getImage());
-        
+
+
+        //set default constraints of gridbag
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.ipadx = 50;
+        c.ipady = 50;
+
+
         //adding items to menu
         menuBar.add(menu);
         
@@ -63,52 +80,97 @@ public class UI extends JFrame implements ActionListener{
         //adding menubar to frame
         setJMenuBar(menuBar);
         
-        //adding ticker
-        add(label);
- 
-        button.addActionListener(this);
-        add(button);
+        //int incrementer and label
+        {
+            //adding ticker
+            c.gridx = 3;
+            c.gridwidth = 3;
+            c.gridy = 0;
+            add(label, c);
+            
+            //adding button incrementer
+            button.addActionListener(this);
+            c.gridx = 0;
+            c.gridy = 0;
+            add(button, c);
+        }
 
-        add(textField);
+        //adding input for changing int
+        {
+            c.gridx = 0;
+            c.gridy = 1;
+            add(textField, c);
 
-        submitText.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                var = Integer.parseInt(textField.getText());
-                label.setText(permString + var);
-            }
-        });
-        add(submitText);
+            submitText.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    var = Integer.parseInt(textField.getText());
+                    label.setText(permString + var);
+                }
+            });
+            c.gridx = 3;
+            c.gridy = 1;
+            add(submitText, c);
+        }
 
-        move.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                move();
-            }
-        });
-        add(move);
+        //adds room input, moving, shooting
+        {
+            c.gridx = 0;
+            c.gridwidth = 2;
+            c.gridy = 2;
+            add(roomInput, c);
 
-        shoot.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                shoot();
-            }
-        });
-        add(shoot);
+            move.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    move(Integer.parseInt(roomInput.getText()));
+                }
+            });
+            c.gridx = 2;
+            c.gridy = 2;
+            add(move, c);
 
-        buyArrows.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                purchaseArrows();
-            }
-        });
-        add(buyArrows);
+            shoot.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    shoot(Integer.parseInt(roomInput.getText()));
+                }
+            });
+            c.gridx = 4;
+            c.gridy = 2;
+            add(shoot, c);
+        }
 
-        buySecrets.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                purchaseSecrets();
-            }
-        });
-        add(buySecrets);
+        //buying arrows and secrets
+        {
+            buyArrows.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    purchaseArrows();
+                }
+            });
+            c.gridx = 0;
+            c.gridwidth = 2;
+            c.gridy = 3;
+            // c.ipadx = 100;
+            add(buyArrows, c);
 
+            c.gridx = 2;
+            c.gridwidth = 1;
+            c.gridy = 3;
+            add(arrowLabel, c);
+
+            buySecrets.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    purchaseSecrets();
+                }
+            });
+            c.gridx = 3;
+            c.gridwidth = 3;
+            c.gridy = 3;
+            add(buySecrets, c);
+
+            
+        }
+
+        //set frame to visible
         setVisible(true);
-
 
     }
 
@@ -130,8 +192,8 @@ public class UI extends JFrame implements ActionListener{
         System.out.println("starting new game");
     }
 
-    public void move(){
-        System.out.println("player moving");
+    public void move(int room){
+        System.out.println("player moving to " + room);
     }
 
     public void displayNearbyRooms(){
@@ -146,12 +208,14 @@ public class UI extends JFrame implements ActionListener{
         System.out.println("Displaying hazards");
     }
 
-    public void shoot(){
-        System.out.println("shooting");
+    public void shoot(int room){
+        System.out.println("shooting to " + room);
     }
 
     public void purchaseArrows(){
         System.out.println("buy arrows");
+        arrows++;
+        arrowLabel.setText("Arrows: " + arrows);
     }
 
     public void purchaseSecrets(){
