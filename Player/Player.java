204686cf10583354796686bced763e0649f6246a
Pavan Anoop <1095891@apps.nsd.org>
2024-04-05 13:34:02 -0700

    labels

--- Player/Player.java
+++ Player/Player.java
@@ -25,6 +25,8 @@ package Player;
 import java.util.Random;
 
 public class Player {
+
+// PROPERTIES ------------------------------------------------------------------------
     private int arrows;
     private int goldCoins;
     private int turns;
@@ -33,14 +35,16 @@ public class Player {
     int[] pitPos;
     int[] batPos;
     Random rand = new Random();
-    int int_random = rand.nextInt(6);
+    int int_random = rand.nextInt(5);
 
+// CONSTRUCTOR -------------------------------------------------------------------------------
     public Player() {
         this.arrows = 3; // Initial number of arrows
         this.goldCoins = 0;
         this.turns = 0;
     }
 
+// METHODS ------------------------------------------------------------------------------------
     public int getArrows() {
         return arrows;
     }
@@ -74,13 +78,12 @@ public class Player {
     }
 
     public String getSecret(int int_rand) {
-        String[] secret = new String[6];
+        String[] secret = new String[5];
         secret[0] = "Their is a bat in room " + batPos;
         secret[1] = "Their is a pit in room " + pitPos;
         secret[2] = "Their is a wumpus in room " + wumpusPos;
         secret[3] = "You are in room " + playerPos;
-        secret[4] = "Trivia answer ##";
-        secret[5] = "The Wumpus is two rooms away";
+        secret[4] = "Trivia answer: ";
         return secret[int_rand];
     }
 
