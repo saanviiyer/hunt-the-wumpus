ff03c580c3dc0a7232e7f8895a61e47b4949bc2e
kenntnisse <2003817@apps.nsd.org>
2024-05-09 13:23:42 -0700

    removed some unnecessary pathfinding

--- Cave/Cave.java
+++ Cave/Cave.java
@@ -36,10 +36,7 @@ public class Cave {
       {{d,j,k,a,h,l}, {i,h,f,g,b,k}}
     };*/
     
-    boolean[][][] openings = { // [row][col][dir]
-      {{true,false,true,false,true,false}, {false,true,false,true,false,true}},
-      {{false,false,true,true,true,false}, {true,true,false,false,false,true}}
-    }; // connected to row, alternating column
+
 
 
     boolean[][] paths = new boolean[30][6];
@@ -47,7 +44,6 @@ public class Cave {
     //   6 cols, 5 rows
     public Cave() {
         //this.randomOpen();
-        if (RAND.nextInt(3) != 0) this.connectByColumn();
         //sets adjacencies
         for (int i = 0; i < 30; i++){
             int row = i/6; // [0,4]
@@ -91,142 +87,8 @@ public class Cave {
     }
 
     
-    public void randomOpen(){
-      // a-l
-      // a, b, c, d, e, f = 3
-      // g, e, l, i, j, c = 3
-      // d, j, k, a, h, l = 3
-      // i, h, f, g, b, k = 3
-      boolean a = RAND.nextBoolean();
-      boolean b = RAND.nextBoolean();
-      boolean c = RAND.nextBoolean();
-      boolean d = RAND.nextBoolean();
-      boolean e = RAND.nextBoolean();
-      boolean f = RAND.nextBoolean();
-      boolean g = RAND.nextBoolean();
-      boolean h = RAND.nextBoolean();
-      boolean i = RAND.nextBoolean();
-      boolean j = RAND.nextBoolean();
-      boolean k = RAND.nextBoolean();
-      boolean l = RAND.nextBoolean();
-
-      this.openings[0][0][0] = a;
-      this.openings[0][0][1] = b;
-      this.openings[0][0][2] = c;
-      this.openings[0][0][3] = d;
-      this.openings[0][0][4] = e;
-      this.openings[0][0][5] = f;
-
-      this.openings[0][1][0] = g;
-      this.openings[0][1][1] = e;
-      this.openings[0][1][2] = l;
-      this.openings[0][1][3] = i;
-      this.openings[0][1][4] = j;
-      this.openings[0][1][5] = c;
-
-      this.openings[1][0][0] = d;
-      this.openings[1][0][1] = j;
-      this.openings[1][0][2] = k;
-      this.openings[1][0][3] = a;
-      this.openings[1][0][4] = h;
-      this.openings[1][0][5] = l;
-
-      this.openings[1][1][0] = i;
-      this.openings[1][1][1] = h;
-      this.openings[1][1][2] = f;
-      this.openings[1][1][3] = g;
-      this.openings[1][1][4] = b;
-      this.openings[1][1][5] = k;
-      /*
-      this.openings = { // [row][col][dir]
-        //n, ne, se, s, sw, nw
-        {{a,b,c,d,e,f}, {g,e,l,i,j,c}},
-        {{d,j,k,a,h,l}, {i,h,f,g,b,k}}
-      }; */
-    }
-    public void connectByColumn(){
-      // a-l
-      // a, b, c, d, e, f = 3
-      // g, e, l, i, j, c = 3
-      // d, j, k, a, h, l = 3
-      // i, h, f, g, b, k = 3
-
-      boolean a = true;
-      boolean b = false;
-      boolean c = false;
-      boolean d = true;
-      boolean e = false;
-      boolean f = false;
-      boolean g = true;
-      boolean h = false;
-      boolean i = true;
-      boolean j = false;
-      boolean k = false;
-      boolean l = false;
-
-      // one of b, c, e, f is true;
 
 
-      switch (RAND.nextInt(4)){
-        case 0:
-          b = true;
-          break;
-        case 1:
-          c = true;
-          break;
-        case 2:
-          e = true;
-          break;
-        case 3:
-          f = true;
-          break;
-        default: break;
-      }
-      if (b){
-        l = true;
-      } else if (c){
-        h = true;
-      } else if (e) {
-        k = true;
-      } else if (f){
-        j = true;
-      }
-
-
-      this.openings[0][0][0] = a;
-      this.openings[0][0][1] = b;
-      this.openings[0][0][2] = c;
-      this.openings[0][0][3] = d;
-      this.openings[0][0][4] = e;
-      this.openings[0][0][5] = f;
-
-      this.openings[0][1][0] = g;
-      this.openings[0][1][1] = e;
-      this.openings[0][1][2] = l;
-      this.openings[0][1][3] = i;
-      this.openings[0][1][4] = j;
-      this.openings[0][1][5] = c;
-
-      this.openings[1][0][0] = d;
-      this.openings[1][0][1] = j;
-      this.openings[1][0][2] = k;
-      this.openings[1][0][3] = a;
-      this.openings[1][0][4] = h;
-      this.openings[1][0][5] = l;
-
-      this.openings[1][1][0] = i;
-      this.openings[1][1][1] = h;
-      this.openings[1][1][2] = f;
-      this.openings[1][1][3] = g;
-      this.openings[1][1][4] = b;
-      this.openings[1][1][5] = k;
-      /*
-      this.openings = { // [row][col][dir]
-        //n, ne, se, s, sw, nw
-        {{a,b,c,d,e,f}, {g,e,l,i,j,c}},
-        {{d,j,k,a,h,l}, {i,h,f,g,b,k}}
-      }; */
-    }
 
     public void open(int id, int dir){
       this.paths[id][dir] = true;
@@ -289,27 +151,10 @@ public class Cave {
     }
     public boolean isNextTo(int id){ // if there is a path there
       return this.isNextTo(loc.getPlayerPos(), id);
-      /*
-      int r = loc.getPlayerPos()/6;
-      int c = loc.getPlayerPos()%6;
-      for (int i = 0; i < 6; i++)
-        if (this.openings[r%2][c%2][i] && this.adj[loc.getPlayerPos()][i] == id) return true;
-      return false;
-      /*
-      for (int i: this.adj[loc.getPlayerPos()]) if (i == id) return true;
-      return false;*/
+
     }
     public void goTo(int id){
-      /* 
-      System.out.println(loc.getPlayerPos());
-      for(int i: this.adj[loc.getPlayerPos()]) this.hexes[i].reset();
-      this.hexes[loc.getPlayerPos()].reset();
-      if (this.isNextTo(id)) loc.setPlayerPos(id);
-      
-      for(int i = 0; i < 6; i++) if (this.openings[(loc.getPlayerPos()/6)%2][(loc.getPlayerPos()%6)%2][i]) 
-        this.hexes[this.adj[loc.getPlayerPos()][i]].setColor(Hex.GREEN);
-      this.hexes[loc.getPlayerPos()].setColor(Hex.RED);*/
-      //System.out.println(loc.getPlayerPos());
+
       for(int i: this.adj[loc.getPlayerPos()]) this.hexes[i].reset();
       this.hexes[loc.getPlayerPos()].reset();
       if (this.isNextTo(id)) loc.setPlayerPos(id);
@@ -322,7 +167,7 @@ public class Cave {
     public int shoot(int dir, int len){ // dir = [0,5]
       int cur = loc.getPlayerPos();
       for (int i = 0; i < len; i++){
-        if (this.openings[(cur/6)%2][(cur%6)%2][dir]) cur = this.adj[cur][dir];
+        if (this.paths[cur][dir]) cur = this.adj[cur][dir];
       }
       return cur;
     }
