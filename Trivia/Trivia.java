5f59eb91dceb2809c48cfa0f132b0e89f972d84c
saanviiyer <saanvi.iyer@icloud.com>
2024-05-20 13:25:05 -0700

    cleaned up

--- Trivia/Trivia.java
+++ Trivia/Trivia.java
@@ -8,7 +8,7 @@ public class Trivia {
 
     Random r = new Random();
 
-    Question[] questions = new Question[5];
+    Question[] questions = new Question[6];
 
     public Trivia() {
         questions[0] = new Question("How many teachers are there at BHS?", "102");
@@ -16,10 +16,11 @@ public class Trivia {
         questions[2] = new Question("Who is the principal of BHS?", "Juan Price");
         questions[3] = new Question("When was BHS built?", "1953");
         questions[4] = new Question("How many times has BHS Science Olympiad won the state championships?", "5");
+        questions[5] = new Question("Who were BHS's first-ever international DECA winners?", "Saanvi, Kaustubh, and Prisha");
     }
 
     public Question getQuestion() {
-        int rand = r.nextInt(questions.size());
+        int rand = r.nextInt(questions.length);
         return questions.get(rand);
     }
 }
