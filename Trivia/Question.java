dcc8cab9fdc4bb37831068fd3a3bfa6ba5937f03
saanviiyer <saanvi.iyer@icloud.com>
2024-05-20 13:10:59 -0700

    started trivia work

---
+++ Trivia/Question.java
@@ -0,0 +1,24 @@
+package Trivia;
+
+public class Question {
+    private String question;
+    private String answer;
+    int points;
+    boolean correct;
+
+    public Question {
+
+    }
+
+    public void setQuestion(String q) {
+        this.question = q;
+    }
+
+    public void setAnswer(String a) {
+        this.answer = a;
+    }
+
+    public void answerQ() {
+        
+    }
+}
