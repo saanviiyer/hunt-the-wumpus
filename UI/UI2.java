141c915a6dc5ff0f563ecc2e319a8494daaa0137
nathanc32 <2030699@apps.nsd.org>
2024-05-25 18:03:31 -0700

    copied over properties and methods of original ui

--- UI/UI2.java
+++ UI/UI2.java
@@ -2,33 +2,166 @@ package UI;
 
 import javax.swing.*;
 
+import Player.Player;
 import net.miginfocom.swing.*;
-
-
+import java.awt.*;
+import java.awt.event.*;
 
 public class UI2 extends JFrame{
     //////////////////////
     //// PROPERTIES  /////
     //////////////////////
+    Player p = new Player();
 
+    JMenuBar menuBar = new JMenuBar();
+    JMenu menu = new JMenu("File");
+    JMenuItem exit = new JMenuItem("Exit");
+    JMenuItem startNewGame = new JMenuItem("New Game");
+
+    
+    JButton shoot = new JButton("Shoot");
+
+    JButton buyArrows = new JButton("Purchase Arrows");
+
+
+
+    JButton buySecrets = new JButton("Purchase Secrets");
+
+    int arrows = p.getArrows();
+    JLabel arrowLabel = new JLabel("Arrows: " + arrows);
+
+    int score = 0;
+    int highScore = 0;
+    JLabel scoreLabel = new JLabel("Score: " + score);
+    JLabel highScoreLabel = new JLabel("High Score: " + highScore);
+
+    JLabel currentPlayerLabel = new JLabel("Player: ");
+    JLabel currentCaveLabel = new JLabel("Cave: ");
+
+    JButton goN = new JButton();
+    JButton goNE = new JButton();
+    JButton goE = new JButton();
+    JButton goSE = new JButton();
+    JButton goS = new JButton();
+    JButton goSW = new JButton();
+    JButton goW = new JButton();
+    JButton goNW = new JButton();
+
+    JPanel miniMap;
+
+    JLabel alerts = new JLabel("Alerts");
     
     //////////////////////
     //// CONSTRUCTOR /////
     //////////////////////
     public UI2(){
         //set frame behavior
-        setTitle("Hunt the Wumpus");
+        setTitle("UI2");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1920,1080);
-        setLayout(null);
-        new MigLayout("wrap 3");
+        setLayout(new MigLayout());
+        
         //change icon of frame
         ImageIcon icon = new ImageIcon("wumpus4.png");
         setIconImage(icon.getImage());
+
+        //add menu and menuitems
+        {
+            //adding items to menu
+            menuBar.add(menu);
+            
+
+            exit.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e){
+                    System.exit(0);
+                }
+            }); 
+            menu.add(exit);
+
+            startNewGame.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e){
+                    startNewGame();
+                }
+            }); 
+            menu.add(startNewGame);
+
+            //adding menubar to frame
+            setJMenuBar(menuBar);
+        }
+
 
         repaint();
         revalidate();
         setResizable(false);
         setVisible(true);
+    }
+
+    private void goNW(){
+        System.out.println("going NW");
+    }
+
+    private void goN(){
+        System.out.println("going N");
+    }
+
+    private void goNE(){
+        System.out.println("going NE");
+    }
+
+    private void goSW(){
+        System.out.println("going SW");
+    }
+
+    private void goS(){
+        System.out.println("going S");
+    }
+
+    private void goSE(){
+        System.out.println("going SE");
+    }
+
+    public void startNewGame(){
+        System.out.println("starting new game");
+    }
+
+    public void move(int room){
+        System.out.println("player moving to " + room);
+    }
+
+    public void displayNearbyRooms(){
+        System.out.println("showing nearby rooms");
+    }
+
+    public void updateHighScore(){
+        System.out.println("updating high score");
+    }
+
+    public void displayHazards(){
+        System.out.println("Displaying hazards");
+    }
+
+    public void shoot(int room){
+        System.out.println("shooting to " + room);
+    }
+
+    public void purchaseArrows(){
+        System.out.println("buy arrows");
+        p.addArrows();
+        arrowLabel.setText("Arrows: " + p.getArrows());
+    }
+
+    public void purchaseSecrets(){
+        System.out.println("buy secrets");
+    }
+
+    public static void changeFont (Component component, Font font ){
+        component.setFont(font);
+        if (component instanceof Container)
+        {
+            for (Component child : ((Container) component).getComponents())
+            {
+                changeFont (child, font);
+            }
+        }
     }
 }
