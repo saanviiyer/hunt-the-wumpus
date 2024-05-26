1ef33672901d768efd005347d956e714bc46f887
nathanc32 <2030699@apps.nsd.org>
2024-05-26 13:31:10 -0700

    removed changing laf

--- UI/UITest.java
+++ UI/UITest.java
@@ -10,10 +10,10 @@ import javax.swing.*;
 class Test {
 
     public static void main(String[] args){
-        try { 
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); 
-            System.out.println("laf changed to " + UIManager.getLookAndFeel());
-        } catch(Exception ignored){}
+        // try { 
+        //     UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); 
+        //     System.out.println("laf changed to " + UIManager.getLookAndFeel());
+        // } catch(Exception ignored){}
         // UI myUI = new UI();
         UI2 myUi2 = new UI2();
     }
