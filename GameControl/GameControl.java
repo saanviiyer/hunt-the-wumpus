a317dfa33fc5f0885e0552622b7230e71f546294
saanviiyer <saanvi.iyer@icloud.com>
2024-06-05 13:52:19 -0700

    comment on trivia

--- GameControl/GameControl.java
+++ GameControl/GameControl.java
@@ -3,6 +3,8 @@
 
 // new change
 // Handles user input (except for High Score and Trivia), coordinates all the other parts of the game.
+// UPDATE: trivia should now work correctly
+
 package GameControl;
 import Player.*;
 import Trivia.*;
