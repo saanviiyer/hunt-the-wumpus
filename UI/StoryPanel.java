9a0570fae31709e81974dafcc6f986af5a6298ab
nathanc32 <158622878+nathanc32@users.noreply.github.com>
2024-06-07 16:16:40 -0700

    fixed not showing right panel when pressing back

--- UI/StoryPanel.java
+++ UI/StoryPanel.java
@@ -30,26 +30,26 @@ public class StoryPanel extends JPanel{
         setBackground(Color.GRAY);
 
         //creates a textbox containing text from an html file
-        JEditorPane tutorial = new JEditorPane();
+        JEditorPane story = new JEditorPane();
         URL text = UI.class.getResource("story.html");
         try {
-            tutorial.setPage(text);
+            story.setPage(text);
         } catch (Exception e) {
             System.out.println("html file not found");
         }
-        tutorial.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, Boolean.TRUE);
-        tutorial.setFont(legendOfZeldaFont.deriveFont(Font.PLAIN, 15));
-        tutorial.setEditable(false);
-        tutorial.setFocusable(false);
-        tutorial.setOpaque(false);
-        JScrollPane tutorialScrollPane = new JScrollPane(tutorial);
+        story.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, Boolean.TRUE);
+        story.setFont(legendOfZeldaFont.deriveFont(Font.PLAIN, 15));
+        story.setEditable(false);
+        story.setFocusable(false);
+        story.setOpaque(false);
+        JScrollPane tutorialScrollPane = new JScrollPane(story);
         tutorialScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         add(tutorialScrollPane, "center, h 500px, w 1000px, push, flowy");
 
         JButton back = new JButton("Back");
         back.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
-                crd.show(UI.getContentPane(), StartPanel.IDENTIFIER);
+                crd.show(UI.getContentPane(), previouslyDisplayedCard);
             }
         });
         back.setBackground(Color.WHITE);
