5626ce773e88a34b5736e19576f50854f1dc3860
nathanc32 <158622878+nathanc32@users.noreply.github.com>
2024-06-07 14:05:18 -0700

    tried code to play sound

--- Sound/Sound.java
+++ Sound/Sound.java
@@ -6,9 +6,13 @@
 // LAST UPDATED: 5/23/2024
 // CURRENT STATUS: runs
 package Sound;
+import java.io.File;
+
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 import javax.swing.JOptionPane;
 
 // coin, falling into a pit, shooting an arrow, wumpus, win/loose, bats, cold draft, hint
@@ -21,9 +25,11 @@ public class Sound{
     System.out.println("SOUND");
   }
 
-  public static void playSound()
-  {
-    // plays an imported sound
+  public static void playSound(String path) throws Exception {
+    AudioInputStream stream = AudioSystem.getAudioInputStream(new File("path/to/your/sound.wav"));
+    Clip clip = AudioSystem.getLine(new DataLine.Info(Clip.class, stream.getFormat()));
+    clip.open(stream);
+    clip.start();
   }
   
 }
