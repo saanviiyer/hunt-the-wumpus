0251135669ad38d87d59f84e271781492128c825
nathanbc32 <2030699@apps.nsd.org>
2024-06-07 19:17:54 -0700

    removed junk

--- Main.java
+++ Main.java
@@ -6,24 +6,18 @@
 
 
 import Player.*;
-import Trivia.*;
 import UI.*;
 
 import java.io.FileNotFoundException;
-
-import javax.swing.UIManager;
 import HighScore.*;
 
 public class Main{
     public static void main(String[] args) throws FileNotFoundException{
         System.out.println("Hello Bumpell");
-
-        //Cave cave = new Cave();
         HighScore highScore = new HighScore();
-        Player player = new Player("");
+        // Player player = new Player("");
         UI ui = new UI();
 
-        //cave.draw(ui);
 
         
     }
